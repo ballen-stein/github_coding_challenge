42fc7c3fb34348d710982612132a95fe4358cd16
bma33 <54296720+bma33@users.noreply.github.com>
2020-02-25 15:16:31 -0500

    Updated API object and repository displays

--- app/src/main/java/com/example/nytcodingchallenge/MainActivity.java
+++ app/src/main/java/com/example/nytcodingchallenge/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -24,6 +23,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
@@ -32,15 +32,11 @@ import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private ApiCall apiCall = new ApiCall("https://api.github.com");
-    private ApiCall apiCall;
-    private String apiUrl, searchedOrg;
+    private String apiUrl, searchedOrg, githubUrl = "https://api.github.com/orgs/";
     private int public_repos = -1, repoPageCount = 1;
     private Context mContext;
 
@@ -61,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void searchListener() {
         searchButton();
-        searchBar();
     }
 
 
@@ -69,42 +64,35 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.searchButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                searchedOrg = searchBar.getText().toString();
-                Log.d("GithubChallengeApp", "Searched organization: " + searchedOrg);
-                public_repos = -1;
-                repoPageCount = 1;
-                orgList = new ArrayList<>();
-
-                apiUrl = "https://api.github.com/users/" + searchedOrg;
-                makeApiCalls(false);
-
-                //apiUrl += "/repos";
-                //apiCall = new ApiCall(apiUrl);
-                //makeApiCall(true);
-
-                //apiUrl = "https://api.github.com/users/" + searchedOrg + "/repos?per_page=100;page="+(i);
-                //System.out.println("https://api.github.com/users/" + searchedOrg + "/repos?per_page=100;page="+i+1);
-                //System.out.println("https://api.github.com/users/" + searchedOrg + "/repos?per_page=100;page="+(i+1));
-                //printRepos();
+                setApiInformation();
             }
         });
     }
 
+    private void setApiInformation(){
+        searchedOrg = searchBar.getText().toString();
+        public_repos = -1;
+        repoPageCount = 1;
+        orgList = new ArrayList<>();
+        apiUrl = githubUrl + searchedOrg;
+
+        makeApiCalls(false);
+    }
+
     private void makeApiCalls(boolean getRepoList){
         Observable<Response> observable = Observable.create(new ObservableOnSubscribe<Response>() {
-            OkHttpClient client = new OkHttpClient();
+            //OkHttpClient client = new OkHttpClient();
 
             @Override
             public void subscribe(@NonNull ObservableEmitter<Response> emitter) throws Throwable {
                 try{
-                    Request request = new Request.Builder()
+                    /*Request request = new Request.Builder()
                             .url(apiUrl)
                             .build();
-                    System.out.println(apiUrl);
 
-                    Response response = client.newCall(request).execute();
+                    Response response = client.newCall(request).execute();*/
+                    Response response = new ApiCall(apiUrl).getResponse();
                     if(response.isSuccessful()){
-
                         emitter.onNext(response);
                     }
                     emitter.onComplete();
@@ -126,15 +114,11 @@ public class MainActivity extends AppCompatActivity {
             public void onNext(Object o) {
                 try {
                     Response response = (Response) o;
-                    //makeRealCall(response.body().string());
-                    //setResponse(response.body().string());
                     if(!getRepoList){
-                        makeRealCall(response.body().string());
+                        getPublicRepoCount(Objects.requireNonNull(response.body()).string());
                     } else {
-                        setResponse(response.body().string());
-                        //public_repos = 0;
+                        parseResponse(Objects.requireNonNull(response.body()).string());
                     }
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -148,30 +132,59 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete() {
                 if(public_repos > 0){
-                    Log.d("GithubChallengeApp", "Starting loop");
-                    apiUrl = "https://api.github.com/users/" + searchedOrg + "/repos?per_page=100;page="+(repoPageCount);
+                    apiUrl = githubUrl + searchedOrg + "/repos?per_page=100;page="+(repoPageCount);
                     repoPageCount++;
                     public_repos-=100;
-                    apiCall = new ApiCall(apiUrl);
+                    new ApiCall(apiUrl);
                     makeApiCalls(true);
                 } else {
-                    //printRepos();
                     setRepos();
                 }
             }
         };
-
         observable.subscribeOn(Schedulers.io())
                 .subscribe(observer);
-
-
     }
+
+
+    private void parseResponse(String response){
+        Moshi moshi = new Moshi.Builder().build();
+        Type type = Types.newParameterizedType(List.class, Organization.class);
+        JsonAdapter<List<Organization>> jsonAdapter = moshi.adapter(type);
+        List<Organization> list;
+        try {
+            list = jsonAdapter.fromJson(response);
+            orgList.addAll(Objects.requireNonNull(list));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void getPublicRepoCount(String response) throws IOException {
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<OrganizationRepoCount> jsonAdapter = moshi.adapter(OrganizationRepoCount.class);
+        OrganizationRepoCount repoCount = jsonAdapter.fromJson(response);
+        public_repos = Objects.requireNonNull(repoCount).getPublic_repos();
+    }
+
 
     private void setRepos() {
         sortReposByStars();
         startDataDisplay();
-        //printRepos();
     }
+
+
+    private void sortReposByStars(){
+        Comparator<Organization> compareByStars = new Comparator<Organization>() {
+            @Override
+            public int compare(Organization org1, Organization org2) {
+                return Integer.compare(org1.getStargazers_count(),org2.getStargazers_count());
+            }
+        };
+        Collections.sort(orgList, compareByStars.reversed());
+    }
+
 
     private void startDataDisplay() {
         try {
@@ -184,119 +197,28 @@ public class MainActivity extends AppCompatActivity {
             });
         } catch (Exception e){
             e.printStackTrace();
-            System.out.println("Error with the layout");
         }
-    }
-
-    private void makeRealCall(String response) throws IOException {
-        //Log.d("GithubChallengeApp", "Response body (String): " + responseBody);
-        Moshi moshi = new Moshi.Builder().build();
-        JsonAdapter<OrganizationRepoCount> jsonAdapter = moshi.adapter(OrganizationRepoCount.class);
-        OrganizationRepoCount repoCount = jsonAdapter.fromJson(response);
-        public_repos = repoCount.getPublic_repos();
-        Log.d("GithubChallengeApp", "Public Repos count: " + public_repos);
-    }
-
-
-    private void makeApiCall(final boolean getListOfRepos) {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("GithubChallengeApp", apiUrl);
-                if(getListOfRepos){
-                    try{
-                        Log.d("GithubChallengeApp", "Getting list of public repos...");
-                        int repoCount = (public_repos/30)+1;
-                        setResponse(apiCall.getResponse());
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                } else {
-                    try {
-                        Log.d("GithubChallengeApp", "Getting repo list count...");
-                        setRepoCount(apiCall.getResponse());
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        thread.start();
-    }
-
-
-    private void setResponse(String response){
-        System.out.println(response);
-        Log.d("GithubChallengeApp", response);
-
-        Moshi moshi = new Moshi.Builder().build();
-        Type type = Types.newParameterizedType(List.class, Organization.class);
-        JsonAdapter<List<Organization>> jsonAdapter = moshi.adapter(type);
-        List<Organization> list;
-
-        try {
-            list = jsonAdapter.fromJson(response);
-            orgList.addAll(list);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Failed due to: " + e);
-        }
-    }
-
-    private void sortReposByStars(){
-        Comparator<Organization> compareByStars = new Comparator<Organization>() {
-            @Override
-            public int compare(Organization org1, Organization org2) {
-                return Integer.compare(org1.getStargazers_count(),org2.getStargazers_count());
-            }
-        };
-        Collections.sort(orgList, compareByStars.reversed());
-    }
-
-    private void printRepos(){
-        Log.d("GithubChallengeApp", "List size: " + orgList.size());
-
-        Comparator<Organization> compareByStars = new Comparator<Organization>() {
-            @Override
-            public int compare(Organization org1, Organization org2) {
-                return Integer.compare(org1.getStargazers_count(),org2.getStargazers_count());
-            }
-        };
-        Collections.sort(orgList, compareByStars.reversed());
-
-        ArrayList<Organization> temp = getSortedRepos();
-
-        try {
-            for(int i = 0; i < temp.size(); i++){
-                Log.d("GithubChallengeApp", i+": " + temp.get(i).getFull_name() + ", " + temp.get(i).getStargazers_count() + ", " + temp.get(i).getHtml_url());
-            }
-        } catch (Exception e){
-            Log.d("GithubChallengeApp", "This organization has less than 3 repos");
-        }
-    }
-
-
-    private void setRepoCount(String response) throws IOException {
-        System.out.println(response);
-        Moshi moshi = new Moshi.Builder().build();
-        JsonAdapter<OrganizationRepoCount> jsonAdapter = moshi.adapter(OrganizationRepoCount.class);
-        OrganizationRepoCount repoCount = jsonAdapter.fromJson(response);
-        public_repos = repoCount.getPublic_repos();
-        Log.d("GithubChallengeApp", "Public Repos count: " + public_repos);
     }
 
 
     public ArrayList<Organization> getSortedRepos(){
         ArrayList<Organization> tempList = new ArrayList<>();
-        for(int i=0; i < 3; i++){
-            tempList.add(orgList.get(i));
+        try{
+            int listTopThree =3;
+            if(orgList.size() < 3 && orgList.size() > 0){
+                listTopThree = public_repos;
+            }
+            for(int i=0; i < listTopThree; i++){
+                tempList.add(orgList.get(i));
+            }
+            findViewById(R.id.noReposFoundFrame).setVisibility(View.GONE);
+        } catch (Exception e){
+            findViewById(R.id.noReposFoundFrame).setVisibility(View.VISIBLE);
+            e.printStackTrace();
         }
         return tempList;
     }
 
-    private void searchBar() {
-        //TODO Add search bar Async/RxJava to search on change
-    }
 
     public void starWebView(String url){
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
--- app/src/main/java/com/example/nytcodingchallenge/apai_connections/ApiCall.java
+++ app/src/main/java/com/example/nytcodingchallenge/apai_connections/ApiCall.java
@@ -1,11 +1,7 @@
 package com.example.nytcodingchallenge.apai_connections;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.IOException;
 
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -19,39 +15,11 @@ public class ApiCall {
 
     private OkHttpClient client = new OkHttpClient();
 
-    private String run() throws IOException {
+    public Response getResponse() throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-
-        /*try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }*/
-        final String[] apiResponse = new String[1];
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(!response.isSuccessful()){
-                    throw new IOException("Unexpected code: " + response);
-                } else {
-                    apiResponse[0] = response.body().toString();
-                }
-            }
-        });
-        return apiResponse[0];
+        return client.newCall(request).execute();
     }
 
-    public String getUrl(){
-        return url;
-    }
-
-    public String getResponse() throws IOException {
-        return run();
-    }
 }
